d1a1dd0f4588227542540b96757fa06c41bf2345
VladiShm <114508981+VladiShm@users.noreply.github.com>
2026-02-24 22:48:51 +0500

    pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,16 @@
+def test_user_login():
+    print("hello")
+
+class TestUserLogin:
+    def test_usr_1(self):
+        print('hello1')
+
+    def test_usr_2(self):
+        print('hello2')
+
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5
